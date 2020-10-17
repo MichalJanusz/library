b0fbe06b5c5b7ee3f6dc742f793363c5b9f8bf21
MichalJanusz <janusz.j.michal@gmail.com>
2020-10-17 16:03:06 +0200

    created book model

--- library/models.py
+++ library/models.py
@@ -6,3 +6,16 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=64)
+    author = models.CharField(max_length=64)
+    pub_date = models.DateField()
+    isbn = models.IntegerField(unique=True)
+    pages = models.IntegerField()
+    cover = models.URLField(blank=True)
+    lang = models.CharField(max_length=16)
+
+    def get_absolute_url(self):
+        return f'/book/{self.id}'
