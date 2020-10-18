b7c609ce0bf44b316cb2ad4c78a1637af6ff72c5
MichalJanusz <janusz.j.michal@gmail.com>
2020-10-18 18:23:36 +0200

    books lissting and filtering done

--- library/models.py
+++ library/models.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import AbstractUser
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
 
@@ -11,10 +12,10 @@ class User(AbstractUser):
 class Book(models.Model):
     title = models.CharField(max_length=64)
     author = models.CharField(max_length=64)
-    pub_date = models.DateField()
-    isbn = models.IntegerField(unique=True)
+    pub_date = models.PositiveIntegerField(default=1, validators=[MinValueValidator(1), MaxValueValidator(9999)])
+    isbn = models.BigIntegerField(unique=True)
     pages = models.IntegerField()
-    cover = models.URLField(blank=True)
+    cover = models.URLField(blank=True, max_length=500)
     lang = models.CharField(max_length=16)
 
     def get_absolute_url(self):
