c223c920ed48b7f30484c52375f0bc6970071f5e
MichalJanusz <janusz.j.michal@gmail.com>
2020-10-18 19:40:53 +0200

    added ISBN validator

--- library/models.py
+++ library/models.py
@@ -1,9 +1,19 @@
 from django.contrib.auth.models import AbstractUser
-from django.core.validators import MinValueValidator, MaxValueValidator
+from django.core.exceptions import ValidationError
+from django.utils.translation import ugettext_lazy as _
+from django.core.validators import MinValueValidator, MaxValueValidator, MaxLengthValidator, MinLengthValidator
 from django.db import models
 
 
 # Create your models here.
+
+def isbn_validator(value):
+    if value not in [10, 13]:
+        raise ValidationError(
+            _('%(value)s is not a valid ISBN number'),
+            params={'value': value},
+        )
+
 
 class User(AbstractUser):
     pass
@@ -13,7 +23,8 @@ class Book(models.Model):
     title = models.CharField(max_length=64)
     author = models.CharField(max_length=64)
     pub_date = models.PositiveIntegerField(default=1, validators=[MinValueValidator(1), MaxValueValidator(9999)])
-    isbn = models.BigIntegerField(unique=True, verbose_name='ISBN')
+    isbn = models.BigIntegerField(unique=True, verbose_name='ISBN',
+                                  validators=[isbn_validator])
     pages = models.IntegerField(verbose_name='Number of pages')
     cover = models.URLField(blank=True, max_length=500, verbose_name='Link to cover')
     lang = models.CharField(max_length=16, verbose_name='Publication language')
