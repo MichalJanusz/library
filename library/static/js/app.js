170ec03723f56252d7d2f0ca6d3c37a5cb75b5f0
MichalJanusz <janusz.j.michal@gmail.com>
2020-10-21 09:13:17 +0200

    added error handling for no pub date and isbn number in ajax

--- library/static/js/app.js
+++ library/static/js/app.js
@@ -1,5 +1,3 @@
-console.log('test')
-
 let table = $('#table-results')
 let csrf_token = $('#csrf_token').val()
 
@@ -10,39 +8,51 @@ function googleBookSearch() {
 
     let results = $('#tbody-results')
     results.html('')
-    console.log(`search ${search}`)
     table.show()
 
     $.ajax({
-        url: `https://www.googleapis.com/books/v1/volumes?q=${search}&maxResults=20`,
+        url: `https://www.googleapis.com/books/v1/volumes?q=${search}&maxResults=20&printType=books`,
         dataType: 'json'
         })
-        .done(function (data){
+        .done(function (data) {
 
-            for(let i = 0; i < data.items.length; i++){
+            for (let i = 0; i < data.items.length; i++) {
+
                 let imageLinks = data.items[i].volumeInfo.imageLinks
                 let thumbnail
-                if (typeof imageLinks !== 'undefined'){
+                if (typeof imageLinks !== 'undefined') {
                     thumbnail = imageLinks.thumbnail
                 } else {
                     thumbnail = ''
                 }
+
                 let pageCount = data.items[i].volumeInfo.pageCount
-                if (typeof pageCount === 'undefined'){
+                if (typeof pageCount === 'undefined') {
                     pageCount = ''
                 }
+
+                let publishedDate = data.items[i].volumeInfo.publishedDate
+                if (typeof publishedDate === 'undefined') {
+                    publishedDate = ''
+                }
+
+                let industryIdentifiers = data.items[i].volumeInfo.industryIdentifiers
+                if (typeof industryIdentifiers === 'undefined') {
+                    continue
+                }
+
                 results.append(
                     `<tr><form method="post" id="import-form${i}"><input name="csrfmiddlewaretoken" form="import-form${i}" type="hidden" value="${csrf_token}"></form>
-            <th scope="row">${i+1}</th>
-            <td><input name="title" type="hidden" form="import-form${i}" readonly class="form-control-plaintext" value="${data.items[i].volumeInfo.title}">${data.items[i].volumeInfo.title}</td>
-            <td><input name="author" type="hidden" form="import-form${i}" readonly class="form-control-plaintext" value="${data.items[i].volumeInfo.authors[0]}">${data.items[i].volumeInfo.authors[0]}</td>
-            <td><input name="pub_date" type="hidden" form="import-form${i}" readonly class="form-control-plaintext" value="${data.items[i].volumeInfo.publishedDate.split('-')[0]}">${data.items[i].volumeInfo.publishedDate.split('-')[0]}</td>
-            <td><input name="isbn" type="hidden" form="import-form${i}" readonly class="form-control-plaintext" value="${data.items[i].volumeInfo.industryIdentifiers[0].identifier.replace(/\D/g, '')}">${data.items[i].volumeInfo.industryIdentifiers[0].identifier.replace(/\D/g, '')}</td>
-            <td><input name="pages" type="hidden" form="import-form${i}" readonly class="form-control-plaintext" value="${pageCount}">${pageCount}</td>
-            <td><img src="${thumbnail}" alt="book cover missing"><input name="cover" type="hidden" form="import-form${i}" value="${thumbnail}"></td>
-            <td><input name="lang" type="hidden" form="import-form${i}" readonly class="form-control-plaintext" value="${data.items[i].volumeInfo.language}">${data.items[i].volumeInfo.language}</td>
-            <td><input type="submit" class="btn btn-success" form="import-form${i}" value="Import"></td>
-        </tr>`
+                     <th scope="row">${i + 1}</th>
+                     <td><input name="title" type="hidden" form="import-form${i}" readonly class="form-control-plaintext" value="${data.items[i].volumeInfo.title}">${data.items[i].volumeInfo.title}</td>
+                     <td><input name="author" type="hidden" form="import-form${i}" readonly class="form-control-plaintext" value="${data.items[i].volumeInfo.authors[0]}">${data.items[i].volumeInfo.authors[0]}</td>
+                     <td><input name="pub_date" type="hidden" form="import-form${i}" readonly class="form-control-plaintext" value="${publishedDate.split('-')[0]}">${publishedDate.split('-')[0]}</td>
+                     <td><input name="isbn" type="hidden" form="import-form${i}" readonly class="form-control-plaintext" value="${industryIdentifiers[0].identifier.replace(/\D/g, '')}">${industryIdentifiers[0].identifier.replace(/\D/g, '')}</td>
+                     <td><input name="pages" type="hidden" form="import-form${i}" readonly class="form-control-plaintext" value="${pageCount}">${pageCount}</td>
+                     <td><img src="${thumbnail}" alt="book cover missing"><input name="cover" type="hidden" form="import-form${i}" value="${thumbnail}"></td>
+                     <td><input name="lang" type="hidden" form="import-form${i}" readonly class="form-control-plaintext" value="${data.items[i].volumeInfo.language}">${data.items[i].volumeInfo.language}</td>
+                     <td><input type="submit" class="btn btn-success" form="import-form${i}" value="Import"></td>
+                </tr>`
                 )
             }
 
