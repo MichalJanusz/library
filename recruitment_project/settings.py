398f374e478341f718ebeee72860d0222945d8cc
MichalJanusz <janusz.j.michal@gmail.com>
2020-10-21 09:14:19 +0200

    Made changes required for heroku

--- recruitment_project/settings.py
+++ recruitment_project/settings.py
@@ -11,18 +11,23 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'kdo7wo=hz#o7k6mpln682qf!f33o5zyr%j5we13dsc218j5a4*'
+# SECRET_KEY = 'kdo7wo=hz#o7k6mpln682qf!f33o5zyr%j5we13dsc218j5a4*'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'kdo7wo=hz#o7k6mpln682qf!f33o5zyr%j5we13dsc218j5a4*')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = []
 
@@ -42,6 +47,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -74,6 +80,19 @@ WSGI_APPLICATION = 'recruitment_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'library',
+        'HOST': 'localhost',
+        'PASSWORD': 'haslo1234',
+        'USER': 'admin',
+        'PORT': 5432
+    }
+}
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
@@ -110,15 +129,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = 'library/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'library/static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 AUTH_USER_MODEL = 'library.User'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
-
-try:
-    from recruitment_project.local_settings import DATABASES
-except ModuleNotFoundError:
-    print("local_settings.py is not configured")
-    print("configure the database settings and try again")
-    exit(0)
